3983b240066ee13f81dab45574bcf46e78c5377d
Ozge <ozonel883@hotmail.com>
2021-01-03 22:10:44 +0000

    search form has binded in the products view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product
+
 
 # Create your views here.
+
+
+def all_products(request):
+    products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please type product name to search")
+                return redirect(reverse('products'))
+            quieries = Q(name_icontains=query) | Q(description_icontains=query)
+            products = products.filter(quieries)
+    context = {
+        'products': products,
+        'search_term': query,
+    }
+    return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
