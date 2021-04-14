f51c45a2c00703b6558151000e6bac1a13a3d635
Ozge <ozonel883@hotmail.com>
2021-04-14 19:58:23 +0000

    index page new arrival views added

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'home/index.html')
+
+    products_newarrival = Product.objects.all().order_by('pk')[:4]
+
+    context = {
+        'product_newarrival': products_newarrival
+    }
+    return render(request, 'home/index.html', context)
