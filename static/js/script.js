4279669d2b766ac4a81018133beb418c0e2a251e
Ozge <ozonel883@hotmail.com>
2021-05-04 20:19:35 +0000

    modal delayed popup

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,7 @@ function delayShow(){ setTimeout(() => {
     $('#newsLetter').modal('show');
     console.log("modal function works");
 }, 3000);}
+/// clear out the delayshow if it is called once(after it called once)
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("Hello! I am loaded");
     });
