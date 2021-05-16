2ff2535c38da776d3f868d081001ca905eddf6d9
Ozge <ozonel883@hotmail.com>
2021-05-16 19:18:32 +0000

    card checking and  stripe payment functionality added

--- checkout/views.py
+++ checkout/views.py
@@ -1,18 +1,39 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
+
+from cart.contexts import cart_contents
+
+import stripe
 
 
 # Create your views here.
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "Your cart is empty at the moment")
         return redirect(reverse('products'))
-    
+
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total*100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, 'stripe public key needs to be set')
+
     context = {
-        'stripe_public_key': 'pk_live_51IqjPKCAbWLyaUT3nGLrvpQTRuhnVw9uqW1ASuBjsNM7nEZTNj6zkOzMCGP6loTcYfng23w6y8YQHnpY3r40jmT600395yHjGb',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
 
     }
-    return render(request,context)
+    return render(request , context)
