fb0d5bd60336c62e307967a8040f450c9ef56d53
Ozge <ozonel883@hotmail.com>
2021-05-17 19:38:01 +0000

    add and adjust product functoins are created

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('add/<item_id>/', views.add_cart, name='add_cart'),
+    path('adjust/<item_id>/', views.adjust_cart, name='adjust_cart'),
 
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -20,5 +20,21 @@ def add_cart(request, item_id):
     else:
         cart[item_id] = quantity
 
-    request.session['cart'] = cart    
+    request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    # Adjust the items in the cart
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            cart.pop[item_id]
+
+    request.session['cart'] = cart
+    return redirect(reverse(view_cart))
