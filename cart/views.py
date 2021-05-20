a06dba84454e1a4489f32989aee3f78f3b094900
Ozge <ozonel883@hotmail.com>
2021-05-20 20:44:46 +0000

    toasts templates are finishing up

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -14,13 +14,14 @@ def view_cart(request):
 def add_cart(request, item_id):
     # Add specified item to the cart
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product,pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to{cart[item_id]}')
     else:
         cart[item_id] = quantity
         messages.success(request, f'Added {product.name} to you cart')
@@ -31,15 +32,17 @@ def add_cart(request, item_id):
 
 def adjust_cart(request, item_id):
     # Adjust the items in cart
-
+    product = get_object_or_404(Product,pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         if quantity > 0:
             cart[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
         else:
             cart.pop(item_id)
+            messages.success(request, f'{cart[item_id]} sucessfully removed')
 
     request.session['cart'] = cart
     return redirect(reverse(view_cart))
@@ -48,9 +51,12 @@ def adjust_cart(request, item_id):
 def remove_cart(request, item_id):
     # Remove the items from cart
     try:
+        product = get_object_or_404(Product,pk=item_id)
         cart = request.session.get('cart', {})
         cart.pop(item_id)
         request.session['cart'] = cart
+        messages.success(request, f'{cart[item_id]} sucessfully removed')
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request,f'Error removing the item {e}')
         return HttpResponse(status=500)
