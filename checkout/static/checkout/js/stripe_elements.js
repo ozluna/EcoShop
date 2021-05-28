f6766ab9894e26a1131dcb4bee7f7428ad4bc486
Ozge <ozonel883@hotmail.com>
2021-05-28 15:38:05 +0000

    setting uo the stripe api on the project

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,9 +1,9 @@
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1,-1);
 var clientSecret = $('#id_client_secret').text().slice(1,-1);
-var stripe = Stripe(stripe_public_key);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
-var style = {
-    base: {
+var style= {
+      base: {
         color: '#000',
         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
         fontSmoothing: 'antialiased',
@@ -16,18 +16,18 @@ var style = {
         color: '#dc3545',
         iconColor: '#dc3545'
     }
-};
-var card = elements.create('card', {style:style});
+    };
+var card = elements.create('card',{style:style});
 card.mount('#card-element');
 
 // error handles on the card element
     card.addEventListener('change', function(event){
     var errorDiv = document.getElementById('card-errors');
     if (event.error){
-        var html = <span class ="icon" role="alert">
-                <i class="fas fa-times"></i>
-            </span>
-            <span>${event.error.message}</span>
+        var html = `<span class ="icon" role="alert">
+                        <i class="fas fa-times"></i>
+                   </span>
+                   <span>${event.error.message}</span>`;
 
         $(errorDiv).html(html);    
     }
