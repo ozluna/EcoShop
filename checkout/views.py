671ed86c59e8fd90036fc9c5220eb5cf6db90db1
Ozge <ozonel883@hotmail.com>
2021-05-30 21:06:56 +0000

    checkout page payment intetn caching is added

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -8,6 +9,25 @@ from products.models import Product
 from cart.contexts import cart_contents
 
 import stripe
+import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, the payment cannot be processed at the moment. \
+            please try again later.')
+        return HttpResponse(content=e, status=400)
+
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -33,7 +53,7 @@ def checkout(request):
             for item_id, item_data in cart.items():
                 try:
                     product = Product.objects.get(id=item_id)
-                     
+                                         
                     order_line_item = OrderLineItem(
                             order=order,
                             product=product,
@@ -64,8 +84,8 @@ def checkout(request):
         stripe_total = round(total*100)
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY
+            amount=stripe_total,
+                currency=settings.STRIPE_CURRENCY,
         )          
         order_form = OrderForm()
     if not stripe_public_key:
