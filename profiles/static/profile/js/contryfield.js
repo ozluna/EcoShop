e1d4b6b2c92d9cc807cf8ea01844271ee1437b3e
Ozge <ozonel883@hotmail.com>
2021-06-11 20:36:36 +0000

    adjust the color pallette

--- profiles/static/profile/js/contryfield.js
+++ profiles/static/profile/js/contryfield.js
@@ -1,12 +1,12 @@
 let countryfield = $('#id_default_country').val();
 if(!countrySelected) {
-    $('#id_default_country').css('color', '#aab7c4');
+    $('#id_default_country').css('color', '#83C5BE');
 };
 $('#id_default_country').change(function() {
     countrySelected = $(this).val();
     if(!countrySelected) {
-        $(this).css('color', '#aab7c4');
+        $(this).css('color', '#83C5BE');
     } else {
-        $(this).css('color', '#000');
+        $(this).css('color', '#006D77');
     }
 });
