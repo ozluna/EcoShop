ae655e48dbc7200646abe2738f4c7dbbe1170008
Ozge <ozonel883@hotmail.com>
2021-06-12 19:40:44 +0000

    confirmation emails texts added and fixed typos and errors

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -4,7 +4,7 @@ var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style= {
       base: {
-        color: '#000',
+        color: '#006D77',
         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
         fontSmoothing: 'antialiased',
         fontSize: '16px',
