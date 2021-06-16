e7689031ef896fe37cd96313b417542df60f3bf7
Ozge <ozonel883@hotmail.com>
2021-06-16 19:52:57 +0000

    SECRET_KEY has updated

--- EcoShop/settings.py
+++ EcoShop/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j3n81^vkaz9a$mk1i@6b9q0sle-$-_n((sb%=bd4_q2+yqt^$5'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ecoshop-oz.herokuapp.com', 'localhost']
 
