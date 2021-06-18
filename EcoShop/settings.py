d73aae6cd00f3030fd2e912be0d4f4fce40f127b
Ozge <ozonel883@hotmail.com>
2021-06-18 20:15:20 +0000

    try to fix my deployment issue

--- EcoShop/settings.py
+++ EcoShop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '2xm@i$0sw+-@(9ptuy8y(tu%)u3@snmn-_as7q$&c#+rnhlszu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
